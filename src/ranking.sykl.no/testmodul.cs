92cd528713d55de4498b4e03797412f0e609127d
vidarls <vidar.l.somme@gmail.com>
2012-06-24 22:54:14 +0200

    Initial commit

---
+++ src/ranking.sykl.no/testmodul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ranking.sykl.no
+{
+    public class testmodul : Nancy.NancyModule
+    {
+        public testmodul()
+        {
+            Get["/"] = _ =>  "<h1>Hurra for ranking.sykl.no</h1>";
+        }
+
+    }
+}
