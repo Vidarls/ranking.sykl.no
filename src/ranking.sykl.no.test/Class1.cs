b6a0ef78071db346179865244a26980b40d910b7
vidarls <vidar.l.somme@gmail.com>
2012-06-24 23:46:25 +0200

    Test scaffolding and failing testtest
    Failng test test to test appharbor autotesting

--- src/ranking.sykl.no.test/Class1.cs
+++ src/ranking.sykl.no.test/Class1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ranking.sykl.no.test
 {
-    public class Class1
+    public class SimplestThing
     {
+        [Fact]
+        public void Test_appharbor_testrunner()
+        {
+            Assert.True(false);
+        }
     }
 }
